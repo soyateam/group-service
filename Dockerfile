0c144f9f53ba7ff6aa3ac2a75ec8d0ccd8400a36
nettashviro <netta.zhviro@gmail.com>
2020-08-09 13:56:24 +0300

    build: change docker file stages and loggers

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,18 @@
-###--- STAGE 1 - Builder ---###
-FROM node:12-alpine as BUILD
+###--- STAGE 1 - Development ---###
+FROM node:12-alpine as DEV
 WORKDIR /usr/src/app
 COPY package*.json ./
 RUN npm install
+
+###--- STAGE 2 - Build ---###
+FROM node:12-alpine as BUILD
+WORKDIR /usr/src/app
+COPY --from=DEV /usr/src/app/package*.json ./
+COPY --from=DEV /usr/src/app/node_modules ./node_modules
 COPY . .
 RUN npm run build
 
-###--- STAGE 2 - Production ---###
+###--- STAGE 3 - Production ---###
 FROM node:12-alpine as PROD 
 ENV NODE_ENV=prod
 WORKDIR /usr/src/app
--- src/utils/logger/logger.ts
+++ src/utils/logger/logger.ts
@@ -1,30 +1,20 @@
-import * as winston from "winston";
-import config from "../../config";
-import { SeverityLevel } from "./severityLevel";
+import * as winston from 'winston';
+import config from '../../config';
+import { SeverityLevel } from './severityLevel';
 
 export const logger = winston.createLogger({
   defaultMeta: { service: config.server.name },
+  format: winston.format.combine(winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), winston.format.json()),
 });
 
 if (process.env.NODE_ENV !== config.env.prod) {
   // Console handler
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.combine(
-        winston.format.timestamp({
-          format: "YYYY-MM-DD HH:mm:ss",
-        }),
-        winston.format.json()
-      ),
-    })
-  );
+  logger.add(new winston.transports.Console());
 } else {
   // File handler
   logger.configure({
-    level: "info",
-    transports: [
-      new winston.transports.File({ filename: "./logs/hierarchy/service.log" }),
-    ],
+    level: 'info',
+    transports: [new winston.transports.File({ filename: './logs/hierarchy/service.log' })],
   });
 }
 
@@ -37,14 +27,7 @@ if (process.env.NODE_ENV !== config.env.prod) {
  * @param user - the user requesting for the service.
  * @param meta - additional optional information.
  */
-export const log = (
-  severity: SeverityLevel,
-  name: string,
-  description: string,
-  correlationId?: string,
-  user?: string,
-  more?: object
-) => {
+export const log = (severity: SeverityLevel, name: string, description: string, correlationId?: string, user?: string, more?: object) => {
   logger.log({
     name,
     correlationId,
