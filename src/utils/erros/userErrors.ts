24e23992e6afd0e6a9c82e39f190720b0fb88eb1
nettashviro <netta.zhviro@gmail.com>
2020-08-11 22:07:23 +0300

    refactor: rename exception

--- src/utils/erros/userErrors.ts
+++ src/utils/erros/userErrors.ts
@@ -1,37 +1,37 @@
-import { UserError } from './errorTypes';
+import { UserError } from "./errorTypes";
 
 export class IdInvalidError extends UserError {
-  constructor(message?: string) {
-    super(message || 'Id is invalid', 400);
+  constructor(message = "Id is invalid") {
+    super(message, 400);
   }
 }
 
 export class QueryParamInvalidError extends UserError {
-  constructor(message?: string) {
-    super(message || 'Query param is invalid', 400);
+  constructor(message = "Query param is invalid") {
+    super(message, 400);
   }
 }
 
-export class OrganizationNotFound extends UserError {
-  constructor(message?: string) {
-    super(message || 'Organization not found', 404);
+export class GroupNotFound extends UserError {
+  constructor(message = "Group not found") {
+    super(message, 404);
   }
 }
 
 export class AuthenticationError extends UserError {
-  constructor(message?: string) {
-    super(message || 'Authentication Error', 401);
+  constructor(message = "Authentication Error") {
+    super(message, 401);
   }
 }
 
 export class NotPermittedError extends UserError {
-  constructor(message?: string) {
-    super(message || 'Operation not permitted', 403);
+  constructor(message = "Operation not permitted") {
+    super(message, 403);
   }
 }
 
 export class NotFoundError extends UserError {
-  constructor(message?: string) {
-    super(message || 'Resource not found', 404);
+  constructor(message = "Resource not found") {
+    super(message, 404);
   }
 }
