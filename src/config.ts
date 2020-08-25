1c758d4733a11705b98110dcab4062e38792e4f2
nettashviro <netta.zhviro@gmail.com>
2020-08-25 10:41:28 +0300

    build: change connection string

--- src/config.ts
+++ src/config.ts
@@ -5,11 +5,7 @@ const config = {
     name: 'group-Service',
   },
   db: {
-    connectionString: `mongodb://${
-      process.env.DB_USERNAME != undefined && process.env.DB_PASSWORD != undefined
-        ? `${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@`
-        : ''
-    }${process.env.DB_SERVER || 'mongo'}:${process.env.DB_PORT || 27017}/${process.env.DB_NAME || 'groupDB'}`,
+    connectionString: `mongodb://${process.env.MONGO_CONNECTION_STRING}/${process.env.GROUP_DB_NAME}`,
   },
   env: {
     prod: 'prod',
@@ -17,10 +13,6 @@ const config = {
     node: process.env.NODE_ENV || 'dev',
   },
   RootAncestorId: process.env.ROOT_ANCESTOR_ID || '5e56858e4203fc40043591a5',
-  authentication: {
-    key: process.env.AUTHENTICATION_PUBLIC_KEY || 'publickey',
-    required: process.env.AUTHENTICATION_REQUIRED || false,
-  },
 };
 
 export default config;
