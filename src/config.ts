cd9d13075276962edaa3ec31bfe4d2589ef8e7d3
nettashviro <netta.zhviro@gmail.com>
2020-08-26 13:00:02 +0300

    fix: change connection string mondo

--- src/config.ts
+++ src/config.ts
@@ -7,8 +7,7 @@ const config = {
   db: {
     connectionString: process.env.MONGO_CONNECTION_STRING || 'mongodb://mongo:27017',
     logs: {
-      connectionStringLogs:
-        process.env.MONGO_CONNECTION_STRING_LOGS || 'mongodb://admin:Aa123456@ds063789.mlab.com:63789/groups',
+      connectionStringLogs: process.env.MONGO_CONNECTION_STRING_LOGS || 'mongodb://mongo:27017/logs',
       expiredInSec: 2592000, // 30 days
     },
   },
